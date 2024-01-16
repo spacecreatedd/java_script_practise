221a3451480bc5a42ffed7f46f82688e6880c60a
spacecreatedd <kimzhenya1313@mail.ru>
2024-01-16 18:04:01 +0700

    second

--- js/main.js
+++ js/main.js
@@ -3,5 +3,9 @@ let app = new Vue({
     data: {
         product: "Socks",
         desc: " A pair of warm, fuzzy socks. ",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        linkText: "More products like this",
     }
 })
