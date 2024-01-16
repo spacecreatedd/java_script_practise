50a03acca59ba62c7a13f768d8bd7a0d394c79ed
spacecreatedd <kimzhenya1313@mail.ru>
2024-01-16 18:08:16 +0700

    third

--- js/main.js
+++ js/main.js
@@ -7,5 +7,7 @@ let app = new Vue({
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         linkText: "More products like this",
+        onSale: true,
+        inventory: 100,
     }
 })
