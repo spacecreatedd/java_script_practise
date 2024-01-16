eba77b9cc226d858341e94cc3e7c83614a795eaf
spacecreatedd <kimzhenya1313@mail.ru>
2024-01-16 18:28:10 +0700

    sixth

--- js/main.js
+++ js/main.js
@@ -2,12 +2,11 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        brand: "Vue Mastery",
         desc: " A pair of warm, fuzzy socks. ",
-        image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         linkText: "More products like this",
-        inStock:true,
         onSale: true,
         inventory: 100,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
@@ -16,25 +15,45 @@ let app = new Vue({
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10,
+                variantSale: true,
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0,
+                variantSale: false,
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart:0,
+        selectedVariant: 0,
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index
+            console.log(index);
         },
         removeFromCart() {
             this.cart -= 1
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        onSale(){
+            return this.variants[this.selectedVariant].variantSale;
         }
     }
 })
